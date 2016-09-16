8b3136c35179fc6ecbed474ceea87fb340c1844d
Garllon <palluthe.bennet@gmail.com>
2016-09-16 20:00:30 +0200

    add footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
+
+  resources :pages, only: [] do
+    collection do
+      get :impressum
+      get :contact
+      get :privacy
+      get :data_protection
+    end
+  end
 end
